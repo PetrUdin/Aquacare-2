956295b5b9b7b837b217e64eff1eb7e89eafdc4d
PetrUdin <udin.petr@gmail.com>
2021-09-09 19:08:05 +0300

    E

---
+++ build/js/all.js
@@ -0,0 +1,96 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scripts/lib.js":
+/*!****************************!*\
+  !*** ./src/scripts/lib.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__(a, b) {\n  return a / b;\n}\n;//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyaXB0cy9saWIuanMuanMiLCJtYXBwaW5ncyI6Ijs7OztBQUVDLDZCQUFlLG9DQUFVQSxDQUFWLEVBQWFDLENBQWIsRUFBZ0I7QUFDNUIsU0FBT0QsQ0FBQyxHQUFHQyxDQUFYO0FBQ0g7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL1JhbmRvbS8uL3NyYy9zY3JpcHRzL2xpYi5qcz8zMWVlIl0sInNvdXJjZXNDb250ZW50IjpbIiAgXHJcblxyXG4gZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gKGEsIGIpIHtcclxuICAgIHJldHVybiBhIC8gYjtcclxufTsiXSwibmFtZXMiOlsiYSIsImIiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/scripts/lib.js\n");
+
+/***/ }),
+
+/***/ "./src/scripts/some.js":
+/*!*****************************!*\
+  !*** ./src/scripts/some.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _lib_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lib.js */ \"./src/scripts/lib.js\");\n\nvar privet = [1, 2, 3, 4, 5, 6];\nconsole.log((0,_lib_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(4, 2));\nconsole.log((0,_lib_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(544, 2));\nconsole.log(privet);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyaXB0cy9zb21lLmpzLmpzIiwibWFwcGluZ3MiOiI7O0FBQUE7QUFFQSxJQUFJQyxNQUFNLEdBQUcsQ0FBQyxDQUFELEVBQUcsQ0FBSCxFQUFLLENBQUwsRUFBTyxDQUFQLEVBQVMsQ0FBVCxFQUFXLENBQVgsQ0FBYjtBQUNBQyxPQUFPLENBQUNDLEdBQVIsQ0FBWUgsbURBQU0sQ0FBQyxDQUFELEVBQUksQ0FBSixDQUFsQjtBQUVBRSxPQUFPLENBQUNDLEdBQVIsQ0FBWUgsbURBQU0sQ0FBQyxHQUFELEVBQUssQ0FBTCxDQUFsQjtBQUVDRSxPQUFPLENBQUNDLEdBQVIsQ0FBWUYsTUFBWiIsInNvdXJjZXMiOlsid2VicGFjazovL1JhbmRvbS8uL3NyYy9zY3JpcHRzL3NvbWUuanM/N2QxZCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgZGl2aWRlIGZyb20gJy4vbGliLmpzJ1xyXG5cclxubGV0IHByaXZldCA9IFsxLDIsMyw0LDUsNl1cclxuY29uc29sZS5sb2coZGl2aWRlKDQsIDIpKTtcclxuXHJcbmNvbnNvbGUubG9nKGRpdmlkZSg1NDQsMikpXHJcblxyXG4gY29uc29sZS5sb2cocHJpdmV0KTtcclxuXHJcbiAgIl0sIm5hbWVzIjpbImRpdmlkZSIsInByaXZldCIsImNvbnNvbGUiLCJsb2ciXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/scripts/some.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/scripts/some.js");
+/******/ 	
+/******/ })()
+;
